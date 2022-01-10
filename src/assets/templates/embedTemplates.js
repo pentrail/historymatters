a78b9f2606baf5ddf245ca58ff61bb6c5cedcf74
Adam <adam@pc>
2022-01-10 15:57:57 -0500

    Added success and response templates

--- src/assets/templates/embedTemplates.js
+++ src/assets/templates/embedTemplates.js
@@ -9,6 +9,25 @@ function permissionDeniedEmbed(description) {
     return permissionDenied;
 }
 
+function successEmbed(description) {
+    const success = new MessageEmbed()
+        .setColor('')
+        .setTitle("Success!")
+        .setDescription(description)
+        .setTimestamp();
+    return success;
+}
+
+function responseEmbed(title) {
+    const response = new MessageEmbed()
+        .setColor('')
+        .setTitle(title)
+        .setTimestamp();
+    return response;
+}
+
 module.exports = {
-    permissionDenied: permissionDeniedEmbed
+    permissionDenied: permissionDeniedEmbed,
+    success: successEmbed,
+    response: responseEmbed
 }
